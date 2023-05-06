3624cf7f367d9d195b65c059e593fe14c16f3b1b
quarzasiphix <quarrysiphix@gmail.com>
2023-05-06 20:12:35 +0200

    actually works

--- victim/src/main.cpp
+++ victim/src/main.cpp
@@ -7,10 +7,11 @@ int main() {
 
 
 	while (myVariable != 0) {
+		system("cls");
 		std::cout << ". addres: " << pMyVariable << std::endl;
 		std::cout << "myVariable: " << myVariable << std::endl;
-		std::cin.get();
-		system("cls");
+		while (myVariable == 42) Sleep(10);
+		Sleep(200);
 	}
 
 	printf("myvar = 0, bye\n");
